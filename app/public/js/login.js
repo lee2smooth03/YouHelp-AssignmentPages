1066704478294f369d82a275dba5cf4d81a538b4
Arthur L. Rozier IV <a.rozier4@gmail.com>
2017-08-30 00:11:45 -0400

    signup page added with some js functionality

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -1,4 +1,4 @@
-$("#submit").on("click", function(e){
+$(".btn-default").on("click", function(e){
 
     e.preventDefault();
     console.log("Write a little code; Test it");
@@ -6,14 +6,50 @@ $("#submit").on("click", function(e){
     // build an object to be sent over
 
     var data = {
-        uName: $('#name').val().trim(),
-        pWord: $('#chirp').val().trim(),
+        uName: $('#uName').val().trim(),
+        pWord: $('#pWord').val().trim(),
         created_at: Date()
     }
 
     console.log(data);
-    $.post("/postChirp", data, function(data){
+
+    $.post("/login", data, function(data){
         console.log(data);
-        getChirp();
+        
     });
+});
+
+$(".btn-info").on("click", function(e){
+
+    e.preventDefault();
+    console.log("Write a little code; Test it");
+
+    // build an object to be sent over
+
+    var pass1 = $('#pWord1').val().trim();
+    var pass2 = $('#pWord2').val().trim();
+
+    if (pass1 === pass2)
+        {
+
+            var data = {
+                uName: $('#uName').val().trim(),
+                pWord: $('#pWord').val().trim(),
+                created_at: Date()
+            }
+
+            console.log(data);
+
+            $.post("/login", data, function(stuff){
+                console.log(stuff);
+                pass1 = "";
+                pass2 = "";
+            });
+        }
+    else
+        {
+            alert("Your Passwords don't match! Try again...");
+                pass1 = "";
+                pass2 = "";
+        }
 });
